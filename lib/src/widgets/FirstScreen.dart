d2ef8317bd8ffd724e01c63d4f4663c87ef4707a
Kioshi Okamoto Rojas <kioshi.okamoto@trade.ec>
2022-05-28 17:38:11 -0500

    feat: add navigation and examples

---
+++ lib/src/widgets/FirstScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              // Within the `FirstScreen` widget
+              onPressed: () {
+                // Navigate to the second screen using a named route.
+                Navigator.pushNamed(context, '/second');
+              },
+              child: const Text('To second screen'),
+            ),
+            ElevatedButton(
+              // Within the `FirstScreen` widget
+              onPressed: () {
+                // Navigate to the second screen using a named route.
+                Navigator.pushNamed(context, '/second');
+              },
+              child: const Text('To third screen'),
+            ),
+            Container(
+              color: Colors.deepOrange,
+              width: 100.0,
+              height: 100.0,
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/src/widgets/SecondScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // Within the SecondScreen widget
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
